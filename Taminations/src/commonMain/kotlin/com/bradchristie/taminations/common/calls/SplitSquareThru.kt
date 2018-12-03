a81da6f204e4f5117e00d59afeb16e9ba2f26b13
Brad Christie <brad@bradchristie.com>
2018-12-02 16:11:57 -0800

    Add code for Left Square Thru, Square Thru from waves, and Split Square Thru Fixes #75 Fixes #57 Fixes #44 Fixes #19

---
+++ Taminations/src/commonMain/kotlin/com/bradchristie/taminations/common/calls/SplitSquareThru.kt
@@ -0,0 +1,54 @@
+package com.bradchristie.taminations.common.calls
+/*
+
+  Taminations Square Dance Animations
+  Copyright (C) 2018 Brad Christie
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+import com.bradchristie.taminations.common.CallContext
+import com.bradchristie.taminations.common.r
+
+class SplitSquareThru(norm: String, name: String) : Action(norm, name) {
+
+  override val requires = listOf("b1/pass_thru","a1/quarter_in","b1/square_thru",
+      "b2/ocean_wave","plus/explode_the_wave","b1/step_thru")
+
+  override fun perform(ctx: CallContext, i: Int) {
+    val (left,right) = if (norm.startsWith("left")) Pair("","Left") else Pair("Left","")
+    val count = norm.takeLast(1).toIntOrNull() ?: 4
+    //  If the centers start, they need to face out to work with the ends
+    //  Otherwise they will face in to work with the other dancers
+    val face = if (ctx.actives.all {
+          d -> d.data.center || (ctx.dancerFacing(d) == null)
+        }) "Out" else "In"
+    ctx.applyCalls("Facing Dancers $right Pass Thru and Face $face",
+                   "$left Square Thru ${count-1}")
+  }
+
+}
+
+class HeadsStart(norm: String, name: String) : Action(norm, name) {
+
+  override fun perform(ctx: CallContext, i: Int) {
+    if (norm.startsWith("head"))
+      ctx.applyCalls("Heads Start")
+    else
+      ctx.applyCalls("Sides Start")
+    ctx.applyCalls(norm.replace("(head|side)start(a)?".r,""))
+  }
+
+}
---
+++ Taminations/src/commonMain/kotlin/com/bradchristie/taminations/common/calls/SquareThru.kt
@@ -0,0 +1,75 @@
+package com.bradchristie.taminations.common.calls
+/*
+
+  Taminations Square Dance Animations
+  Copyright (C) 2018 Brad Christie
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+import com.bradchristie.taminations.common.CallContext
+import com.bradchristie.taminations.common.CallError
+import com.bradchristie.taminations.common.Dancer
+import com.bradchristie.taminations.common.Path
+import com.bradchristie.taminations.common.TamUtils.getMove
+
+class SquareThru(norm: String, name: String) : Action(norm, name) {
+
+  override val requires = listOf("b2/ocean_wave","plus/explode_the_wave","b1/step_thru")
+
+  override fun perform(ctx: CallContext, i: Int) {
+    //  Set up alternating hands
+    val (left,right) = if (norm.startsWith("left"))
+      Pair("","Left-Hand")
+    else
+      Pair("Left-Hand","")
+    //  Find out how many hands
+    val count = norm.takeLast(1).toIntOrNull() ?: 4
+    //  First hand is step to a wave if not already there
+    if (ctx.actives.any { d -> ctx.isInCouple(d) }) {
+      ctx.applyCalls("Facing Dancers Step to a Compact $right Wave")
+      ctx.analyze()
+    }
+    //  Check that wave is the correct hand
+    if (ctx.actives.any { d ->
+              (!d.data.belle && left == "") ||
+              (!d.data.beau && right == "") ||
+              !ctx.isInWave(d)  })
+      throw CallError("Cannot $name from this formation")
+    //  Square thru remaining hands
+    (1 until count).forEach { c ->
+      val hand = if (c % 2 == 0) right else left
+      ctx.applyCalls("Explode and Step to a Compact $hand Wave")
+    }
+    ctx.applyCalls("Step Thru")
+  }
+
+}
+
+class StepToACompactWave(norm: String, name: String) : Action(norm, name) {
+
+  override fun performOne(d: Dancer, ctx: CallContext): Path {
+    val d2 = ctx.dancerFacing(d) ?:
+        throw CallError("Cannot find dancer facing $d")
+    val dist = d.distanceTo(d2)
+    val dir = if (norm.contains("left")) "Right" else "Left"
+    return getMove("Extend $dir").scale(dist/2,0.5)
+  }
+
+  override fun postProcess(ctx: CallContext, i: Int) {
+
+  }
+
+}
