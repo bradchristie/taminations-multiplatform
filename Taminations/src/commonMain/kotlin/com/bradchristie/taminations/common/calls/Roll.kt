447741440cfd9ade7d35212eca082489cf01d9c8
Brad Christie <brad@bradchristie.com>
2019-10-24 15:37:32 -0700

    Don't strip "and" if user enters <anything> And Roll

--- Taminations/src/commonMain/kotlin/com/bradchristie/taminations/common/calls/Roll.kt
+++ Taminations/src/commonMain/kotlin/com/bradchristie/taminations/common/calls/Roll.kt
@@ -24,7 +24,7 @@ import com.bradchristie.taminations.common.CallError
 import com.bradchristie.taminations.common.Dancer
 import com.bradchristie.taminations.common.LevelObject
 
-class Roll : QuarterTurns("roll") {
+class Roll(norm:String,name:String) : QuarterTurns(norm,name) {
 
   override val level = LevelObject("plus")
 
